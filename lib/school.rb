e5d51d47057341f01e88a6e4cd7ead75ccdee1c9
Eric DeSimone <eric.desimone@cortland.edu>
2018-04-28 14:11:07 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,8 +9,8 @@ class School
   end
 
   def add_student(student,grade)
-    @roster[grade] = Array.new if !@roster.any? do |existing_grades|
-      existing_grades == grade
+    @roster[grade] = Array.new if @roster.any? do |existing_grades|
+      existing_grades != grade
     end
     @roster[grade] << student
     binding.pry
