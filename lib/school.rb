0351897005b470477d249d941a227a772b9e4c79
Eric DeSimone <eric.desimone@cortland.edu>
2018-04-28 13:57:23 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,7 +11,7 @@ class School
     @roster[grade] = Array.new if !@roster.any? do |existing_grades|
       existing_grades == grade
     end
-    @roster[grade] << student
+    @roster[grade].push (student)
   end
 
 end
