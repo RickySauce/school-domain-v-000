3297039611c47f0b67158854d89b771d4d237309
Eric DeSimone <eric.desimone@cortland.edu>
2018-04-28 14:52:07 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,15 +9,17 @@ class School
   end
 
   def add_student(student,grade)
+    #creates a new grade if it doesn't already exist
     @roster[grade] = [] unless @roster.any? do |existing_grades|
       existing_grades == grade
     end
+    #adds student to the grade array
     @roster.each do |grades|
       grades.each do |classes|
-        classes << student if grades == grade
+        if grades == grade
+          classes << student
+        end
       end
-    end
-
   end
 
 end
