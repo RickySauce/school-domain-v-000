523ce1b6e54c51c4946af61e2840146fa4ab03a6
Eric DeSimone <eric.desimone@cortland.edu>
2018-04-28 15:07:45 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,7 +12,7 @@ class School
     #creates a new grade if it doesn't already exist
     @roster[grade] = Array.new unless @roster.any? do |existing_grades,classes|
       existing_grades == grade
-      @roster[grade][classes] << student
+      @roster[grade] << student
     end
     #adds student to the grade array
     @roster[grade] << student
